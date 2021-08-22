eeed5e429af1619fed8b3bb70423b6039b50fe9a
TimotheeGimbert <timothee.gimbert@free.fr>
2021-08-22 21:34:00 +0200

    header pure CSS with slide down effect

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 // Function that opens and closes the navbar
 var navOpen = false;
 function OpenNav() {
-    document.getElementById("Nav-list").style.height = "200px";
+    document.getElementById("Nav-list").style.height = "260px";
 }
 function CloseNav() {  
     document.getElementById("Nav-list").style.height = "0";
@@ -18,3 +18,23 @@ function Nav() {
     }
 }
 
+
+//Get the button:
+mybutton = document.getElementById("ArrowUP");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+} 
