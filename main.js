cb315c1d687896e48d29e2c35f07c6f831a50370
TimotheeGimbert <timothee.gimbert@free.fr>
2021-08-23 18:53:11 +0200

    index.scss is cleaned up

--- main.js
+++ main.js
@@ -18,23 +18,8 @@ function Nav() {
     }
 }
 
-
-//Get the button:
-mybutton = document.getElementById("ArrowUP");
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
+// When the user clicks on the up arrow at the bottom of the page, it scrolls all to the top of the page
+function topFunction() { 
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 } 
