dd8ee5c8e3c6426905eff410137f80189f714b8e
TimotheeGimbert <timothee.gimbert@free.fr>
2021-08-25 17:34:43 +0200

    code cleaning done

--- main.js
+++ main.js
@@ -2,20 +2,20 @@
 // Function that opens and closes the navbar
 var navOpen = false;
 function OpenNav() {
-    document.getElementById("Nav-list").style.height = "190px";
+  document.getElementById("Nav-list").style.height = "190px";
 }
 function CloseNav() {  
-    document.getElementById("Nav-list").style.height = "0";
+  document.getElementById("Nav-list").style.height = "0";
 }
 function Nav() {
-    if (navOpen === false) {
-        OpenNav();
-        navOpen = true;
-    }
-    else {
-        CloseNav();
-        navOpen = false;
-    }
+  if (navOpen === false) {
+    OpenNav();
+    navOpen = true;
+  }
+  else {
+    CloseNav();
+    navOpen = false;
+  }
 }
 
 // When the user clicks on the up arrow at the bottom of the page, it scrolls all to the top of the page
